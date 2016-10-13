e2c1ea1837e215f5a217c09f057f669d4901800a
DHclly <335121817@qq.com>
2016-10-14 00:15:39 +0800

    修改单链表

--- Csharp.DataStructure.LinarList/SingleLinkedList.cs
+++ Csharp.DataStructure.LinarList/SingleLinkedList.cs
@@ -1,96 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Csharp.DataStructure.LinarList.Interface;
 
-namespace LinarList
+namespace Csharp.DataStructure.LinarList
 {
     /// <summary>
-    /// 
+    /// 单链表
     /// </summary>
     /// <typeparam name="T"></typeparam>
     class SingleLinkedList<T> : ILinarList<T>
     {
+        public class Snode<U>
+        {
+            /// <summary>
+            /// 数据域
+            /// </summary>
+            public U Data { get; set; }
+            /// <summary>
+            /// 引用域
+            /// </summary>
+            public Snode<U> Next { get; set; }
+
+        }
+        #region Implementation of ILinarList<T>
+
         /// <summary>
-        /// 
+        /// 插入节点
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        private class SNode<T>
-        {
-            /// <summary>
-            /// 
-            /// </summary>
-            public T Data { get; set; }
-
-            /// <summary>
-            /// 
-            /// </summary>
-            public SNode<T> Next { get; set; }
-
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="data"></param>
-            /// <param name="next"></param>
-            public SNode(T data, SNode<T> next)
-            {
-                this.Data = data;
-                this.Next = next;
-            }
-
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="next"></param>
-            public SNode(SNode<T> next)
-            {
-                this.Next = next;
-            }
-
-            /// <summary>
-            /// 
-            /// </summary>
-            public SNode()
-            {
-                this.Data = default(T);
-                this.Next = null;
-            }
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteNode(int position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetLength()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <param name="node"></param>
+        /// <param name="position"></param>
         public void InsertNode(T node, int position)
         {
             throw new NotImplementedException();
         }
 
-        public bool IsEmpty()
+        /// <summary>
+        /// 在末尾插入节点
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public void InsertNode(T node)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 删除节点
+        /// </summary>
+        /// <param name="position"></param>
+        public void DeleteNode(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 通过位置搜索节点
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public T SearchNode(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 通过结点内容搜索节点
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public T SearchNode(T node)
         {
             throw new NotImplementedException();
         }
 
-        public T SearchNode(int position)
+        /// <summary>
+        /// 获得长度
+        /// </summary>
+        /// <returns></returns>
+        public int GetLength()
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// 清空
+        /// </summary>
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 判断是否为空
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
