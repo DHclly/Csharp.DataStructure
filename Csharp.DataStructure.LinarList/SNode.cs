1fcc370eca546d0853a3e88ed90cf950e5dbb874
DHclly <335121817@qq.com>
2016-10-27 08:19:47 +0800

    实现单链表

---
+++ Csharp.DataStructure.LinarList/SNode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.DataStructure.LinarList
+{
+    /// <summary>
+    /// 单链表结点
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SNode<T>
+    {
+        /// <summary>
+        /// 数据域
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// 下一个节点指针
+        /// </summary>
+        public SNode<T> Next { get; set; }
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        public SNode()
+        {
+            Data = default(T);
+            Next = null;
+        }
+
+        /// <summary>
+        /// 非默认构造函数
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="next">下一个节点</param>
+        public SNode(T data, SNode<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
+
+        /// <summary>
+        /// 非默认构造函数
+        /// </summary>
+        /// <param name="next">下一个节点</param>
+        public SNode(SNode<T> next)
+        {
+            Next = next;
+            Data = default(T);
+        }
+
+        /// <summary>
+        /// 非默认构造函数
+        /// </summary>
+        /// <param name="data">数据</param>
+        public SNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+    }
+}
--- Csharp.DataStructure.LinarList/SingleLinkedList.cs
+++ Csharp.DataStructure.LinarList/SingleLinkedList.cs
@@ -184,6 +184,7 @@ namespace Csharp.DataStructure.LinarList
         public void Clear()
         {
             Start = null;
+            Length = 0;
         }
 
         /// <summary>
@@ -196,63 +197,5 @@ namespace Csharp.DataStructure.LinarList
         }
 
         #endregion
-    }
-
-
-    /// <summary>
-    /// 单链表结点
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class SNode<T>
-    {
-        /// <summary>
-        /// 数据域
-        /// </summary>
-        public T Data { get; set; }
-
-        /// <summary>
-        /// 下一个节点指针
-        /// </summary>
-        public SNode<T> Next { get; set; }
-        /// <summary>
-        /// 默认构造函数
-        /// </summary>
-        public SNode()
-        {
-            Data = default(T);
-            Next = null;
-        }
-
-        /// <summary>
-        /// 非默认构造函数
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <param name="next">下一个节点</param>
-        public SNode(T data, SNode<T> next)
-        {
-            Data = data;
-            Next = next;
-        }
-
-        /// <summary>
-        /// 非默认构造函数
-        /// </summary>
-        /// <param name="next">下一个节点</param>
-        public SNode(SNode<T> next)
-        {
-            Next = next;
-            Data = default(T);
-        }
-
-        /// <summary>
-        /// 非默认构造函数
-        /// </summary>
-        /// <param name="data">数据</param>
-        public SNode(T data)
-        {
-            Data = data;
-            Next = null;
-        }
-
     }
 }
